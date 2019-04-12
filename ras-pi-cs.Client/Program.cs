6d89f6dd2874983a8f64c9529c1636cae0c47f07
SUIMA on Ubuntu 18.04 LTS <mizumacrocus@yahoo.co.jp>
2019-04-12 19:06:33 +0900

    first commit

---
+++ ras-pi-cs.Client/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace ras_pi_cs.Client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
